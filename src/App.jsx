65cfb579c8defec94a71d942c871f3334e5637a6
Popa Teodor <teodor.popa96@gmail.com>
2024-09-18 15:03:48 +0300

    lesson 2

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Countries from "./components/Countries";
+import CountrySingle from "./components/CountrySingle";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
+import Layout from "./pages/Layout";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route>
+            {/* This is where other routes will go to allow Layout to be visible everywhere */}
+            <Route path="/" element={<Home />} />
+            <Route path="/countries" element={<Countries />} />
+            <Route path="/countries/:single" element={<CountrySingle />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
